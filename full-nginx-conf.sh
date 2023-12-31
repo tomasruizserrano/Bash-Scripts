ba7c82264e2b2569f59dd2a847f51c15394b2063
Tomás <154073319+tomasruizserrano@users.noreply.github.com>
2023-12-31 20:31:15 +0100

    Update full-nginx-conf.sh

--- full-nginx-conf.sh
+++ full-nginx-conf.sh
@@ -10,8 +10,6 @@ curl -o /etc/nginx/config/general.conf https://raw.githubusercontent.com/tomasru
 curl -o /etc/nginx/config/proxy.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/proxy.conf
 curl -o /etc/nginx/config/security.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/security.conf
 
-cd /etc/nginx/sites-enabled/
-
 curl -o /etc/nginx/sites-avaiable/example.com.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/sites-enabled/example.com.conf
 
 sudo ln -s /etc/nginx/sites-avaiable/example.com.conf /etc/nginx/sites-enabled/example.com.conf 
