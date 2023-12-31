db4a6b7ee5404aaea7633822d21efd517f313064
Tomás <154073319+tomasruizserrano@users.noreply.github.com>
2023-12-31 20:47:54 +0100

    Update full-nginx-conf.sh

--- full-nginx-conf.sh
+++ full-nginx-conf.sh
@@ -2,6 +2,8 @@
 
 apt install nginx nginx-full nginx-extras curl && mkdir /etc/nginx/config
 
+openssl dhparam -out /etc/nginx/dhparam.pem 2048
+
 curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/nginx.conf
 curl -o /etc/nginx/config/general.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/general.conf
 curl -o /etc/nginx/config/proxy.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/proxy.conf
