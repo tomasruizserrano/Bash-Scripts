61867a3f2427cf2b6c34e65f4a4587022957a5b1
Tomás <154073319+tomasruizserrano@users.noreply.github.com>
2023-12-31 20:22:43 +0100

    Update full-nginx-conf.sh

--- full-nginx-conf.sh
+++ full-nginx-conf.sh
@@ -2,18 +2,16 @@
 
 apt install nginx nginx-full nginx-extras curl
 
-cd /etc/nginx && cp nginx.conf nginx.conf.backup
+curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/general.conf
 
-curl -o nginx.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/general.conf
+mkdir /etc/nginx/config
 
-mkdir config && cd config
-
-curl -o general.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/general.conf
-curl -o proxy.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/proxy.conf
-curl -o security.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/security.conf
+curl -o /etc/nginx/config/general.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/general.conf
+curl -o /etc/nginx/config/proxy.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/proxy.conf
+curl -o /etc/nginx/config/security.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/config/security.conf
 
 cd /etc/nginx/sites-enabled/
 
-curl -o example.com.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/sites-enabled/example.com.conf
+curl -o /etc/nginx/sites-enabled/example.com.conf https://raw.githubusercontent.com/tomasruizserrano/Bash-Scripts/main/NGINX.CONF.PRESET/sites-enabled/example.com.conf
 
 sudo ln -s /etc/nginx/sites-enabled/example.com.conf /etc/nginx/sites-avaiable/example.com.conf
